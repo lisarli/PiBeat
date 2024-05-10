7f5939a4edea21f41d9a7d86da1f221f29361861
lisarli <lisaruiwen04@gmail.com>
2024-05-10 17:40:23 -0400

    Modify translation

--- translation.py
+++ translation.py
@@ -1,44 +1,62 @@
-#!/usr/bin/env python
-# Display a runtext with double-buffering.
-from samplebase import SampleBase
-from rgbmatrix import graphics
+import os
 import time
+import board
+import random
+from rgbmatrix import graphics, RGBMatrix, RGBMatrixOptions
 import numpy as np
 import adafruit_mma8451 as acc
-import random
 from threading import Thread
+import pygame
 
+code_run = True
 
-class RunMatrix(SampleBase):
-    def __init__(self, *args, **kwargs):
+class RunMatrix():
+    def __init__(self):
         print("runmatrix starting init")
-        super(RunMatrix, self).__init__(*args, **kwargs)
+        options = RGBMatrixOptions()
+
+        options.hardware_mapping = 'adafruit-hat'
+        options.rows = 16
+        options.cols = 32
+        options.chain_length = 1
+        options.parallel = 1
+        options.row_address_type = 0
+        options.multiplexing = 0
+        options.pwm_bits = 11
+        options.brightness = 100
+        options.pwm_lsb_nanoseconds = 130
+        options.led_rgb_sequence = 'RGB'
+        options.pixel_mapper_config = ''
+        options.panel_type = ''
+        options.gpio_slowdown = 4
+        options.drop_privileges = True
+        options.disable_hardware_pulsing = True
+
+        self.matrix = RGBMatrix(options=options)
+        self.screen = self.matrix.CreateFrameCanvas()
         print("runmatrix done init")
-        self.process()
-        self.offscreen_canvas = self.matrix.CreateFrameCanvas()
 
 
     def run(self):
       global code_run
       while code_run:
-        pos = self.offscreen_canvas.width
+        pos = self.screen.width
 
-        self.offscreen_canvas.Clear()
+        self.screen.Clear()
 
         arrow = acc_translate(acc.PL_PUF)
         instruction = pad_random(arrow)
         extra = np.zeros((27,16,3), dtype=np.uint8)
         led_matrix = np.concatenate((instruction, extra), axis = 0)
 
-        for x in range(self.offscreen_canvas.width):
-            for y in range(self.offscreen_canvas.height):
+        for x in range(self.screen.width):
+            for y in range(self.screen.height):
               r, g, b = led_matrix[x][y]
-              self.offscreen_canvas.SetPixel(x, y, r, g, b)
+              self.screen.SetPixel(x, y, r, g, b)
       
-        self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
-        time.sleep(0.5)
-      
-    
+        self.screen = self.matrix.SwapOnVSync(self.screen)
+        time.sleep(0.5) 
+
 
 def acc_translate(orientation):
   front = (100,100,200)
@@ -103,3 +121,26 @@ def pad_random(arr):
   if offset < 11:
     arr = np.concatenate((arr, back_off), axis = 1)
   return arr
+
+os.putenv('SDL_VIDEODRV','fbcon')
+os.putenv('SDL_FBDEV', '/dev/fb0')
+os.putenv('SDL_MOUSEDRV','dummy')
+os.putenv('SDL_MOUSEDEV','/dev/null')
+os.putenv('DISPLAY','')
+
+pygame.init()
+pygame.mouse.set_visible(False)
+lcd = pygame.display.set_mode()
+
+try:
+  run_text = RunMatrix()
+  t1 = Thread(target=run_text.run)
+  t1.start()
+
+  time.sleep(5)
+finally:
+  print("here")
+  code_run = False
+  t1.join()
+  pygame.quit()
+  print("exiting script")
