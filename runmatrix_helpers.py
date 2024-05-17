07edff27bba2e398a304e22e62421e9e68145f86
Lisa Li <lisaruiwen04@gmail.com>
2024-05-17 06:59:53 -0400

    Write comments for runmatrix helpers

--- runmatrix_helpers.py
+++ runmatrix_helpers.py
@@ -2,6 +2,7 @@ import numpy as np
 import adafruit_mma8451 as acc
 import random
 
+# convert an orientation into a 5x5 matrix containing an arrow with the correct color
 def acc_translate(orientation):
   front = (100,100,200)
   back = (200,0,50)
@@ -25,6 +26,7 @@ def acc_translate(orientation):
   elif orientation == None:
     return np.zeros((5,5,3))
 
+# return a random offset in the valid range to display
 def random_offset():
   return random.randint(0, 11)
 
@@ -51,10 +53,12 @@ def left_arr(color):
 def up_arr(color):
   return down_arr(color)[::-1, ::-1, :]
 
+# get a deterministic hash in valid range for matrix to display
 def get_hash(number):
     hashed_value = hash(number)
     return (hashed_value & 0xFFFFFFFF) % 12
 
+# converts an arrow into a deterministically padded 5x16 matrix
 def pad_random(arr, gen_time):
   offset = get_hash(gen_time) # must be 0-11
   front_off = np.zeros((5,offset,3), dtype=np.uint8)
@@ -65,7 +69,7 @@ def pad_random(arr, gen_time):
     arr = np.concatenate((arr, back_off), axis = 1)
   return arr
   
-
+# returns the current 25x16 matrix corresponding to instructions
 def get_insn_leds(insns, game_time):
     if len(insns)==0:
         return np.zeros((25,16,3), dtype=np.uint8)
@@ -80,6 +84,7 @@ def get_insn_leds(insns, game_time):
     cropped_leds = insn_leds[top_idx-24:top_idx+1]
     return cropped_leds[::-1]
 
+# returns the 6x16 matrix containing feedback about the player's action
 def get_feedback_leds(feedback):
     feedback_leds = np.zeros((6,16,3), dtype=np.uint8)
     feedback_to_color = {
@@ -95,6 +100,7 @@ def get_feedback_leds(feedback):
       feedback_leds[i][:][:] =  c * feedback_to_color[feedback]
     return feedback_leds
 
+# returns a 7x16 matrix containing feedback and target line
 def get_bottom_leds(feedback):
     target_line = np.full((1, 16, 3), 200)
     feedback_leds = get_feedback_leds(feedback)
